7f3388ffc8051952a1e2c32b713c203087e3c710
Jason Xie <elitecqc@gmail.com>
2017-10-24 12:17:11 +0200

    Matrix will now be filled

--- tumo.py
+++ tumo.py
@@ -9,6 +9,7 @@ Created on Fri Oct 20 15:23:41 2017
 #Imports
 import math as m
 import numpy as np
+np.set_printoptions(threshold=np.nan)
 
 #Constants
 rho_w   = 0.01      #[day^-1]
@@ -27,14 +28,14 @@ dr      = R/n       #[cm]
 rc      = np.linspace(0.+0.5*dr,R-0.5*dr,n) #[cm]
 dtheta  = phil/n    #[cm]
 theta   = np.linspace(0.+0.5*dtheta,phil-0.5*dtheta,n)
-A       = np.matrix((int(n**2),int(n**2)))
+A       = np.zeros((int(n**2),int(n**2)))
 
 def left():
     if j == 0:
         Cc = -(dr/(rc[i]*dtheta) + 
                (0.5*dr*dtheta)/dr +  
                rc[i]*rho*dr*dtheta)
-        Cn = dr/(rc[i+int(n)]*dtheta)
+        Cn = dr/(rc[i]*dtheta)
         Ce = ((rc[i+1]*0.5*dr)*dtheta)/dr
         Cs = 0
     elif j == int(n) - 1:
@@ -48,7 +49,7 @@ def left():
         Cc = -(2.*dr/(rc[i]*dtheta) + 
                (0.5*dr*dtheta)/dr +  
                rc[i]*rho*dr*dtheta)
-        Cn = dr/(rc[i+int(n)]*dtheta)
+        Cn = dr/(rc[i]*dtheta)
         Ce = ((rc[i+1]*0.5*dr)*dtheta)/dr
         Cs = dr/(rc[i-int(n)]*dtheta)
     return Cc, Cn, Ce, Cs
@@ -58,7 +59,7 @@ def lower():
         Cc = -(dr/(rc[i]*dtheta) + 
                (0.5*dr*dtheta)/dr + 
                rc[i]*rho*dr*dtheta)
-        Cn = dr/(rc[i+int(n)]*dtheta)
+        Cn = dr/(rc[i]*dtheta)
         Ce = 0
         Cw = ((rc[i-1]*0.5*dr)*dtheta)/dr
     else:
@@ -66,7 +67,7 @@ def lower():
                (0.5*dr*dtheta)/dr + 
                (0.5*dr*dtheta)/dr + 
                rc[i]*rho*dr*dtheta)
-        Cn = dr/(rc[i+int(n)]*dtheta)
+        Cn = dr/(rc[i]*dtheta)
         Ce = ((rc[i+1]*0.5*dr)*dtheta)/dr
         Cw = ((rc[i-1]*0.5*dr)*dtheta)/dr
     return Cc, Cn, Ce, Cw
@@ -78,14 +79,14 @@ def right():
                rc[i]*rho*dr*dtheta)
         Cn = 0
         Cs = dr/(rc[i-int(n)]*dtheta)
-        Cw = ((rc[i-1]-0.5*dr)*dtheta)/dr
+        Cw = ((rc[i-1]*0.5*dr)*dtheta)/dr
     else:
         Cc = -(2.*dr/(rc[i]*dtheta) + 
                (0.5*dr*dtheta)/dr + 
                rc[i]*rho*dr*dtheta)
-        Cn = dr/(rc[i+int(n)]*dtheta)
+        Cn = dr/(rc[i]*dtheta)
         Cs = dr/(rc[i-int(n)]*dtheta)
-        Cw = ((rc[i-1]-0.5*dr)*dtheta)/dr 
+        Cw = ((rc[i-1]*0.5*dr)*dtheta)/dr
     return Cc, Cn, Cs, Cw
 
 def upper():
@@ -94,9 +95,9 @@ def upper():
                (0.5*dr*dtheta)/dr + 
                (0.5*dr*dtheta)/dr + 
                rc[i]*rho*dr*dtheta)
-        Ce = ((rc[i+1]+0.5*dr)*dtheta)/dr
+        Ce = ((rc[i+1]*0.5*dr)*dtheta)/dr
         Cs = dr/(rc[i-int(n)]*dtheta)
-        Cw = ((rc[i-1]-0.5*dr)*dtheta)/dr
+        Cw = ((rc[i-1]*0.5*dr)*dtheta)/dr
     return Cc, Ce, Cs, Cw
 
 def internal():
@@ -104,37 +105,75 @@ def internal():
                (0.5*dr*dtheta)/dr + 
                (0.5*dr*dtheta)/dr + 
                rc[i]*rho*dr*dtheta)
-    Cn = dr/(rc[i+int(n)]*dtheta)
-    Ce = ((rc[i+1]+0.5*dr)*dtheta)/dr
+    Cn = dr/(rc[i]*dtheta)
+    Ce = ((rc[i+1]*0.5*dr)*dtheta)/dr
     Cs = dr/(rc[i-int(n)]*dtheta)
-    Cw = ((rc[i-1]-0.5*dr)*dtheta)/dr
+    Cw = ((rc[i-1]*0.5*dr)*dtheta)/dr
     return Cc, Cn, Ce, Cs, Cw
     
+
 for j in range(0,int(n)):
     for i in range(0,int(n)):
         if abs(rc[i] - R_g) < 0.5*dr or (abs(rc[i] - RI) < 0.5*dr and abs(theta[j] - phi) < 0.5*dtheta):
             rho = rho_avg
-        elif rc[i] < R_g or (rc[i] > RI and theta[j] < phi):
+        elif rc[i] < R_g or (rc[int(i%n)] > RI and theta[int(j%n)] < phi):
             rho = rho_g
         else:
             rho = rho_w
             
         if i == 0:
             (Cc, Cn, Ce, Cs) = left()
-            A[j][i] = Cc
-            A[j][i+int(n)] = Cn
-            A[j][i+1] = Ce
-            A[j][]
+            A[int(j*n)][int(j*n)] = Cc
+
+            if (int(j*n+n) < n**2):
+              A[int(j*n)][int(j*n+n)] = Cn
+
+            if (int(j*n-n) >= 0):
+              A[int(j*n)][int(j*n-n)] = Cs
+
+            A[int(j*n)][int(j*n+1)] = Ce
             
         elif j == 0 and i > 0:
-            lower()
+            (Cc, Cn, Ce, Cw) = lower()
+
+            A[int(j*n+i)][int(j*n+i)] = Cc
+
+            if (int(j*n+i+n) < n**2):
+              A[int(j*n+i)][int(j*n+i+n)] = Cn
+
+            if (int(j*n+i+1) < n**2):
+              A[int(j*n+i)][int(j*n+i+1)] = Ce
+
+            A[int(j*n+i)][int(j*n+i-1)] = Cw
+
 
         elif i == int(n) - 1 and j != 0:
-            right()
+            (Cc, Cn, Cs, Cw) = right()
+            A[int(j*n+i)][int(j*n+i)] = Cc
+
+            if (int(j*n+i+n) < n**2):
+              A[int(j*n+i)][int(j*n+i+n)] = Cn
+
+            if (int(j*n-n+i) >= 0):
+              A[int(j*n+i)][int(j*n-n+i)] = Cs
+
+            A[int(j*n+i)][int(j*n+i-1)] = Cw
             
         elif j == int(n) - 1 and i != 0 and i != int(n) - 1:
-            upper()
+            (Cc, Ce, Cs, Cw) = upper()
+            A[int(j*n+i)][int(j*n+i)] = Cc
+            A[int(j*n+i)][int(j*n+i-n)] = Cs
+            A[int(j*n+i)][int(j*n+i+1)] = Ce
+            A[int(j*n+i)][int(j*n+i-1)] = Cw
         
         else:
-            internal()
-        
+            (Cc, Cn, Ce, Cs, Cw) = internal()
+            A[int(j*n+i)][int(j*n+i)] = Cc
+            A[int(j*n+i)][int(j*n+i+n)] = Cn
+            A[int(j*n+i)][int(j*n+i-n)] = Cs
+            A[int(j*n+i)][int(j*n+i+1)] = Ce
+            A[int(j*n+i)][int(j*n+i-1)] = Cw
+
+# TODO: 
+# - Divide everything in the matrix by Rc
+# - Choose time integration method
